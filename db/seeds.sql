553e52414d9da41be55317c95c607f4338c14627
ginomaglaque <ginomaglaque@gmail.com>
2020-10-24 14:57:15 -0700

    department table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,10 @@
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO department (name)
+VALUES
+    ("Sales"),
+    ("Engineering"),
+    ("Maintence");
+
+INSERT INTO role (title, salary, department_id)
 VALUES
     ("Manager", 200000, 1),
     ("Lead Engineer", 150000, 2),
@@ -6,7 +12,7 @@ VALUES
     ("Test Engineer", 100000, 2),
     ("Janitor", 60000, 3);
 
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
   ('Ronald', 'Firbank', 1, NULL),
   ('Virginia', 'Woolf', 1, NULL),
